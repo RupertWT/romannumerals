8b79a9fe34326a714a5b32be3ce1dcad4e7eb44b
IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>
2016-09-07 14:50:16 +0100

    ...

--- src/main/java/com/rupert/learning/romannumerals/RomanNumerals.java
+++ src/main/java/com/rupert/learning/romannumerals/RomanNumerals.java
@@ -1,20 +1,39 @@
 package com.rupert.learning.romannumerals;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class RomanNumerals 
 {
-    public int fromRomanNumeralsTransformer( String RomanNumerals )
+	
+	private Dictionary<String, Integer> arabicToRomanDictionary() 
+	{
+		
+		Dictionary<String, Integer> arabicToRomanDictionary = new Hashtable<String, Integer>();
+	   
+		arabicToRomanDictionary.put("I", 1);
+		arabicToRomanDictionary.put("V", 5);
+		arabicToRomanDictionary.put("X", 10);
+		arabicToRomanDictionary.put("L", 50);
+		arabicToRomanDictionary.put("C", 100);
+		arabicToRomanDictionary.put("D", 50);
+		arabicToRomanDictionary.put("M", 1000);
+		
+	    return arabicToRomanDictionary;
+	}
+	
+	
+	public int fromRomanNumeralsTransformer( String RomanNumerals )
     {
-    	    	
-    	if (RomanNumerals.equals("V")) {
-    		return 5;
-    	}
-    	
+
+		Dictionary<String, Integer> arabicToRomanDictionary = arabicToRomanDictionary();
+		
     	int Answer = 0;
     	
     	for (int i = 0; i < RomanNumerals.length(); i++) {
-    		if (RomanNumerals.substring(i, i+1).equals("I")) {
-    			Answer += 1;
-    		}
+   		
+   			Answer += arabicToRomanDictionary.get(RomanNumerals.substring(i, i+1));
+   			
     	}
  
     	return Answer;
--- src/test/java/com/rupert/learning/romannumerals/RomanNumeralsTest.java
+++ src/test/java/com/rupert/learning/romannumerals/RomanNumeralsTest.java
@@ -7,25 +7,77 @@ import org.junit.Test;
 public class RomanNumeralsTest {
 	
 	RomanNumerals rn = new RomanNumerals();
-   
+  
+//	Single character values	
+	
 	@Test
     public void I_Returns_One() {
         assertEquals(1, rn.fromRomanNumeralsTransformer("I"));   
     }
 	
 	@Test
-    public void II_Returns_Two() {
-        assertEquals(2, rn.fromRomanNumeralsTransformer("II"));   
-    }
-	
-	@Test
-    public void III_Returns_Three() {
-        assertEquals(3, rn.fromRomanNumeralsTransformer("III"));   
-    }
-
-	@Test
     public void V_Returns_Five() {
         assertEquals(5, rn.fromRomanNumeralsTransformer("V"));   
     }
+	
+	@Test
+    public void X_Returns_Ten() {
+        assertEquals(10, rn.fromRomanNumeralsTransformer("X"));   
+    }
+	
+	
+	@Test
+    public void L_Returns_Fifty() {
+        assertEquals(50, rn.fromRomanNumeralsTransformer("L"));   
+    }
+	
+	
+	@Test
+    public void C_Returns_OneHundred() {
+        assertEquals(100, rn.fromRomanNumeralsTransformer("C"));   
+    }
+	
+	@Test
+    public void D_Returns_FiveHundred() {
+        assertEquals(500, rn.fromRomanNumeralsTransformer("D"));   
+    }
+	
+	@Test
+    public void M_Returns_OneThousand() {
+        assertEquals(1000, rn.fromRomanNumeralsTransformer("M"));   
+    }
+	
+	
+	
+//	Basic adding of character values
+	
+	@Test
+    public void II_Returns_Two() {
+        assertEquals(2, rn.fromRomanNumeralsTransformer("II"));   
+    }
+		
+	@Test
+    public void VIII_Returns_Eight() {
+        assertEquals(8, rn.fromRomanNumeralsTransformer("VIII"));   
+    }
+		
+	@Test
+    public void XI_Returns_Eleven() {
+        assertEquals(11, rn.fromRomanNumeralsTransformer("XI"));   
+    }
+	
+	@Test
+    public void XVI_Returns_Sixteen() {
+        assertEquals(16, rn.fromRomanNumeralsTransformer("XVI"));   
+    }
+	
+
+//	Subtraction of character values	
+	
+	@Test
+    public void IV_Returns_Four() {
+        assertEquals(4, rn.fromRomanNumeralsTransformer("IV"));   
+    }
+	
 	
 }
