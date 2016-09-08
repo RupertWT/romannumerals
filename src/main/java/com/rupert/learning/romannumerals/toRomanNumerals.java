8d5ad0cbb5bce75d041c384538c06ca90f40d403
IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>
2016-09-08 16:27:39 +0100

    ...

--- src/main/java/com/rupert/learning/romannumerals/toRomanNumerals.java
+++ src/main/java/com/rupert/learning/romannumerals/toRomanNumerals.java
@@ -2,9 +2,15 @@ package com.rupert.learning.romannumerals;
 
 public class toRomanNumerals {
 	
-	public String toRomanNumeralsTransformer( int i ) {
+	public String toRomanNumeralsTransformer( int arabicNumbers ) {
 		
-		return "I";
+		String Answer = "";
+		
+		for (int i = 1; i <= arabicNumbers; i++) {
+			Answer += "I";
+		}
+		
+		return Answer;
     	
     }
 
--- src/test/java/com/rupert/learning/romannumerals/toRomanNumeralsTest.java
+++ src/test/java/com/rupert/learning/romannumerals/toRomanNumeralsTest.java
@@ -9,8 +9,23 @@ public class toRomanNumeralsTest {
 	toRomanNumerals rn = new toRomanNumerals();
 	
 	@Test
-    public void I_Returns_One() {
+    public void One_Returns_I() {
         assertEquals("I", rn.toRomanNumeralsTransformer(1));   
+    }
+	
+	@Test
+    public void Two_Returns_II() {
+        assertEquals("II", rn.toRomanNumeralsTransformer(2));   
+    }
+	
+	@Test
+    public void Three_Returns_III() {
+        assertEquals("III", rn.toRomanNumeralsTransformer(3));   
+    }
+	
+	@Test
+    public void Four_Returns_IV() {
+        assertEquals("IV", rn.toRomanNumeralsTransformer(4));   
     }
 	
 }
